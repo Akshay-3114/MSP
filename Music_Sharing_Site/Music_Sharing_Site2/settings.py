f747e40be0aae4a43be0d1d7e3244ce65565c5a6
Akshay-3114 <akshaysuresh492@gmail.com>
2023-06-20 20:13:31 +0530

    trying with changed urls

--- Music_Sharing_Site/Music_Sharing_Site2/settings.py
+++ Music_Sharing_Site/Music_Sharing_Site2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(1rx(%c2^0a(doe0^6c6*f4vzc$3l(2r5der05fowrdes@y#wc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*.vercel.app','.vercel.app' '127.0.0.1']
+ALLOWED_HOSTS = ['https://music-sharing-portal-eta.vercel.app/','https://music-sharing-portal-git-main-akshay-3114.vercel.app/','https://music-sharing-portal-ps0komads-akshay-3114.vercel.app/', '127.0.0.1']
 
 
 # Application definition
